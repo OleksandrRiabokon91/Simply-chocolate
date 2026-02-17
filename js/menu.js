46e2d9643bc351d9dad6fa81e2ecd3339a934b7e
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2026-02-17 18:23:32 +0200

    feat: Finish header for mobile. Mobile menu is ready and working

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.modal.addEventListener("click", (e) => {
+    const link = e.target.closest('a[href^="#"]');
+    if (!link) return;
+
+    e.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetEl = document.getElementById(targetId);
+
+    if (targetEl) {
+      targetEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    refs.modal.classList.remove("is-open");
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
